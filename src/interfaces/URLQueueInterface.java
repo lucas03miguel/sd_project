c6c371b82c5ee4d7d09baaa3af37569733e472a8
Lucas <lucas.m.loberto@gmail.com>
2024-03-26 18:25:12 +0000

    interfaces

---
+++ src/interfaces/URLQueueInterface.java
@@ -0,0 +1,10 @@
+package src.interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface URLQueueInterface extends Remote {
+    String takeLink() throws RemoteException;
+    void offerLink(String link) throws RemoteException;
+    boolean isempty() throws RemoteException;
+}

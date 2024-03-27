3681749530a458847a3628f34af8d9343e54d088
Lucas <lucas.m.loberto@gmail.com>
2024-03-27 02:54:25 +0000

    teste

--- src_teste/nuno/DownloaderInterface.java
+++
@@ -1,9 +0,0 @@
-package src_teste.nuno;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface DownloaderInterface extends Remote{
-    public void downloadWebPage(String url) throws RemoteException;
-    public boolean Heartbeat() throws RemoteException;
-}

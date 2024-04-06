725148b1018a07afe4bfba2e4a1370020437e13c
Lucas <lucas.m.loberto@gmail.com>
2024-04-06 03:12:37 +0100

    é isso velho

--- src/RMIClient.java
+++ src/RMIClient.java
@@ -166,7 +166,6 @@ public class RMIClient extends UnicastRemoteObject {
                                        break;
                                     }
                                 }
-                                
                             }
 
                         }
